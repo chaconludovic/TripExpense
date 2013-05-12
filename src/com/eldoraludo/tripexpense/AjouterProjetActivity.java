ac844ed58b2864a50da2583a2d46b4c43c5ab2fb
chaconludovic <ludovic_chacon@hotmail.com>
2013-05-12 15:57:04 +0200

    ajout d'un menu action

--- src/com/eldoraludo/tripexpense/AjouterProjetActivity.java
+++ src/com/eldoraludo/tripexpense/AjouterProjetActivity.java
@@ -3,9 +3,6 @@ package com.eldoraludo.tripexpense;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,8 +31,7 @@ public class AjouterProjetActivity extends Activity {
 			Projet projet = databaseHandler.trouverLeProjet(idProjet);
 			nomProjetText.setText(projet.getNom());
 		}
-		// Show the Up button in the action bar.
-		setupActionBar();
+
 	}
 
 	public void onClick(View view) {
@@ -58,53 +54,6 @@ public class AjouterProjetActivity extends Activity {
 			setResult(RESULT_OK, i);
 			super.finish();
 		}
-
-		// } else if (backButton.isPressed()) {
-		// // When back button is pressed
-		// // Create an intent
-		// Intent intent = new Intent(this, MainActivity.class);
-		// // Start activity
-		// startActivity(intent);
-		// // Finish this activity
-		// this.finish();
-		//
-		// // Close the database
-		// dao.close();
-		// }
-
-	}
-
-	/**
-	 * Set up the {@link android.app.ActionBar}.
-	 */
-	private void setupActionBar() {
-
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.ajouter_projet, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 
 }
--- src/com/eldoraludo/tripexpense/GestionProjetActivity.java
+++ src/com/eldoraludo/tripexpense/GestionProjetActivity.java
@@ -85,8 +85,7 @@ public class GestionProjetActivity extends ListActivity {
 			}
 
 		});
-		// Show the Up button in the action bar.
-		setupActionBar();
+
 	}
 
 	@Override
@@ -139,45 +138,12 @@ public class GestionProjetActivity extends ListActivity {
 		}
 	}
 
-	/**
-	 * Set up the {@link android.app.ActionBar}.
-	 */
-	private void setupActionBar() {
-
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-
-	}
-
 	public void onClick(View view) {
 		// If add button was clicked
 		if (ajouterNouveauProjetButton.isPressed()) {
 			Intent intent = new Intent(this, AjouterProjetActivity.class);
 			startActivityForResult(intent, REQUEST_AJOUTER_PROJET);
 		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.gestion_projet, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
 	}
 
 }
