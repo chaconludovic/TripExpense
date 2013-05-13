da0b750aca24b4e329ceb69c45dfa5f32cc1036f
chaconludovic <ludovic_chacon@hotmail.com>
2013-05-13 23:12:03 +0200

    fix prob calcul

--- src/com/eldoraludo/tripexpense/SyntheseActivity.java
+++ src/com/eldoraludo/tripexpense/SyntheseActivity.java
@@ -36,19 +36,147 @@ public class SyntheseActivity extends ListActivity {
 		databaseHandler = new DatabaseHandler(this);
 
 		this.setListAdapter(new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, getResultat()));
+				android.R.layout.simple_list_item_1, getListeDettes()));
 
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
 
-	private List<String> getResultat() {
+	private List<String> getListeDettes() {
 		List<Depense> depenses = databaseHandler.getAllDepense(idProjet);
 		List<Participant> participants = databaseHandler
 				.getAllParticipant(idProjet);
 
 		Map<DateTime, Set<Participant>> dateParticipantMap = this
 				.buildDateParticipantMap(participants);
+
+		Map<Depense, Map<Participant, Double>> depenseParticipantsMap = extractionParticipantParDepense(
+				depenses, participants, dateParticipantMap);
+
+		Map<Participant, Map<Participant, Double>> dette = extractionDetteParParticipant(depenseParticipantsMap);
+
+		return extractionListeDette(dette);
+
+	}
+
+	private List<String> extractionListeDette(
+			Map<Participant, Map<Participant, Double>> dette) {
+		List<String> res = new ArrayList<String>();
+		for (Participant participant : dette.keySet()) {
+			for (Participant depenseur : dette.get(participant).keySet()) {
+				String ress = participant.getNom() + " doit à "
+						+ depenseur.getNom() + " la somme de "
+						+ dette.get(participant).get(depenseur);
+				res.add(ress);
+			}
+		}
+		return res;
+	}
+
+	private Map<Participant, Map<Participant, Double>> extractionDetteParParticipant(
+			Map<Depense, Map<Participant, Double>> depenseParticipantsMap) {
+		Map<Participant, Map<Participant, Double>> dette = new HashMap<Participant, Map<Participant, Double>>();
+		for (Depense depense : depenseParticipantsMap.keySet()) {
+			Participant depenseur = databaseHandler
+					.trouverLeParticipant(depense.getParticipantId());
+			Map<Participant, Double> map = depenseParticipantsMap.get(depense);
+			for (Participant participant : map.keySet()) {
+				if (!dette.containsKey(participant)) {
+					dette.put(participant, new HashMap<Participant, Double>());
+				}
+				if (!dette.get(participant).containsKey(depenseur)) {
+					dette.get(participant).put(depenseur, 0.0);
+				}
+				dette.get(participant).put(
+						depenseur,
+						dette.get(participant).get(depenseur)
+								+ map.get(participant));
+			}
+		}
+		// nettoyage
+		Map<Participant, Map<Participant, Double>> detteFinal = copieDeLaMap(dette);
+		for (Participant participant : dette.keySet()) {
+			if (!detteFinal.containsKey(participant)) {
+				continue;
+			}
+			for (Participant depenseur : dette.get(participant).keySet()) {
+				if (!detteFinal.containsKey(depenseur)) {
+					continue;
+				}
+				if (dette.containsKey(depenseur)
+						&& dette.get(depenseur).containsKey(participant)) {
+					Double montantDuParLeDepenseurAuParticipant = dette.get(
+							depenseur).get(participant);
+					if (this.estDetteDuDepenseSuperieurADetteDuParticipant(
+							dette, participant, depenseur,
+							montantDuParLeDepenseurAuParticipant)) {
+						this.recalculDeLaDetteDuDepenseur(dette, detteFinal,
+								participant, depenseur,
+								montantDuParLeDepenseurAuParticipant);
+					} else {
+						this.recalculDeLaDetteDuParticipant(dette, detteFinal,
+								participant, depenseur,
+								montantDuParLeDepenseurAuParticipant);
+					}
+				}
+			}
+		}
+		return detteFinal;
+	}
+
+	private Map<Participant, Map<Participant, Double>> copieDeLaMap(
+			Map<Participant, Map<Participant, Double>> dette) {
+		Map<Participant, Map<Participant, Double>> detteFinal = new HashMap<Participant, Map<Participant, Double>>();
+		for (Participant participant : dette.keySet()) {
+			Map<Participant, Double> mapDepenseur = new HashMap<Participant, Double>();
+			for (Participant depenseur : dette.get(participant).keySet()) {
+				mapDepenseur.put(depenseur,
+						dette.get(participant).get(depenseur));
+			}
+			detteFinal.put(participant, mapDepenseur);
+		}
+		return detteFinal;
+	}
+
+	private void recalculDeLaDetteDuParticipant(
+			Map<Participant, Map<Participant, Double>> dette,
+			Map<Participant, Map<Participant, Double>> detteFinal,
+			Participant participant, Participant depenseur,
+			Double detteDuDepenseur) {
+		Double detteDuParticipant = dette.get(participant).get(depenseur);
+		Double detteRecalcule = detteDuParticipant - detteDuDepenseur;
+		detteFinal.get(participant).put(depenseur, detteRecalcule);
+		detteFinal.get(depenseur).remove(participant);
+		if (detteFinal.get(depenseur).size() == 0) {
+			detteFinal.remove(depenseur);
+		}
+	}
+
+	private void recalculDeLaDetteDuDepenseur(
+			Map<Participant, Map<Participant, Double>> dette,
+			Map<Participant, Map<Participant, Double>> detteFinal,
+			Participant participant, Participant depenseur,
+			Double detteDuDepenseur) {
+		Double detteDuParticipant = dette.get(participant).get(depenseur);
+		Double detteRecalcule = detteDuDepenseur - detteDuParticipant;
+		detteFinal.get(depenseur).put(participant, detteRecalcule);
+		detteFinal.get(participant).remove(depenseur);
+		if (detteFinal.get(participant).size() == 0) {
+			detteFinal.remove(participant);
+		}
+	}
+
+	private boolean estDetteDuDepenseSuperieurADetteDuParticipant(
+			Map<Participant, Map<Participant, Double>> dette,
+			Participant participant, Participant depenseur,
+			Double montantDuParLeDepenseurAuParticipant) {
+		return montantDuParLeDepenseurAuParticipant > dette.get(participant)
+				.get(depenseur);
+	}
+
+	private Map<Depense, Map<Participant, Double>> extractionParticipantParDepense(
+			List<Depense> depenses, List<Participant> participants,
+			Map<DateTime, Set<Participant>> dateParticipantMap) {
 		Map<Depense, Map<Participant, Double>> depenseParticipantsMap = new HashMap<Depense, Map<Participant, Double>>();
 		for (Depense depense : depenses) {
 			DateTime dateDebut = depense.getDateDebut();
@@ -89,36 +217,7 @@ public class SyntheseActivity extends ListActivity {
 			}
 			depenseParticipantsMap.put(depense, mapPart);
 		}
-
-		Map<Participant, Map<Participant, Double>> dette = new HashMap<Participant, Map<Participant, Double>>();
-		for (Depense depense : depenseParticipantsMap.keySet()) {
-			Participant depenseur = databaseHandler
-					.trouverLeParticipant(depense.getParticipantId());
-			Map<Participant, Double> map = depenseParticipantsMap.get(depense);
-			for (Participant participant : map.keySet()) {
-				if (!dette.containsKey(participant)) {
-					dette.put(participant, new HashMap<Participant, Double>());
-				}
-				if (!dette.get(participant).containsKey(depenseur)) {
-					dette.get(participant).put(depenseur, 0.0);
-				}
-				dette.get(participant).put(
-						depenseur,
-						dette.get(participant).get(depenseur)
-								+ map.get(participant));
-			}
-		}
-		List<String> res = new ArrayList<String>();
-		for (Participant participant : dette.keySet()) {
-			for (Participant depenseur : dette.get(participant).keySet()) {
-				String ress = participant.getNom() + " doit à "
-						+ depenseur.getNom() + " la somme de "
-						+ dette.get(participant).get(depenseur);
-				res.add(ress);
-			}
-		}
-		return res;
-
+		return depenseParticipantsMap;
 	}
 
 	public Map<DateTime, Set<Participant>> buildDateParticipantMap(
